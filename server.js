bbf3fd4f7957ef171225ddc3330320e44a3e6a87
João Lucas <joaolucasbittencourt01@gmail.com>
2026-03-26 11:18:44 -0300

    fiz alterações nos ícones e adicionei um favicon

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ const { Pool } = require('pg');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const { Pool } = require('pg');
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.RENDER ? false : { rejectUnauthorized: false }
+    connectionString: process.env.DATABASE_URL
 });
 
 app.use(cors());
